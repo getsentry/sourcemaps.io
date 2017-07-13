491b5f5d6f946268728c124c4a8bde1d290c3b64
Ben Vinegar <ben@benv.ca>
2017-07-13 15:10:18 -0700

    Now handles [X-]SourceMap headers

--- server/lib/validate.js
+++ server/lib/validate.js
@@ -8,16 +8,38 @@ const {
   InvalidSourceMapFormatError
 } = require('./errors');
 
+function getSourceMapLocation(response, body) {
+  // First, look for Source Map HTTP headers
+  const sourceMapHeader =
+    response.headers['x-sourcemap'] || response.headers['sourcemap'];
+
+  if (sourceMapHeader) return sourceMapHeader;
+
+  // If no headers, look for a sourceMappingURL directive on the last line
+  const lines = body.split(/\n/);
+  if (!lines.length > 0) {
+    return null;
+  }
+
+  const last = lines[lines.length - 1];
+  const match = last.match(/sourceMappingURL\=(\S+)$/);
+  if (!match) {
+    return null;
+  }
+
+  return match[1];
+}
+
 function resolveSourceMappingURL(sourceUrl, sourceMappingURL) {
   const urlBase = sourceUrl.replace(/\/[^\/]+$/, '');
-  return sourceMappingURL.startsWith('http') 
+  return sourceMappingURL.startsWith('http')
     ? sourceMappingURL
     : urljoin(urlBase, sourceMappingURL);
 }
 
 function getSourceFile(url, callback) {
   const errors = [];
-  request(url, function (error, response, body) {
+  request(url, function(error, response, body) {
     if (error) {
       console.log(error);
       return;
@@ -28,34 +50,26 @@ function getSourceFile(url, callback) {
       return void callback(errors);
     }
 
-    const lines = body.split(/\n/);
-    if (!lines.length > 0) {
-      console.log('no lines');
-      return;
-    }
-
-    const last = lines[lines.length - 1];
-    const match = last.match(/sourceMappingURL\=(\S+)$/)
-    if (!match) {
-      errors.push(new SourceMapNotFoundError(url))
+    const sourceMappingURL = getSourceMapLocation(response, body);
+    if (!sourceMappingURL) {
+      errors.push(new SourceMapNotFoundError(url));
       return void callback(errors);
     }
 
-    const sourceMappingURL = match[1];
-    const resolvedSourceMappingURL = resolveSourceMappingURL(url, sourceMappingURL)
+    const resolvedSourceMappingURL = resolveSourceMappingURL(url, sourceMappingURL);
 
-    getSourceMap(resolvedSourceMappingURL, function (sourceMapErrors) {
+    getSourceMap(resolvedSourceMappingURL, function(sourceMapErrors) {
       if (sourceMapErrors && sourceMapErrors.length) {
         errors.push.apply(errors, sourceMapErrors);
       }
-      callback(errors)
+      callback(errors);
     });
   });
 }
 
 function getSourceMap(url, callback) {
   const errors = [];
-  request(url, function (error, response, body) {
+  request(url, function(error, response, body) {
     if (error) {
       return void console.log(error);
     }
--- server/test/test.js
+++ server/test/test.js
@@ -14,87 +14,99 @@ const host = 'https://example.org';
 const path = '/static/app.js';
 const url = `${host}${path}`;
 
-describe('validate', function () {
-  it('should download both source files and source maps', function (done) {
-    nock(host)
-      .get(path)
-      .reply(200, '//#sourceMappingURL=app.js.map');
+describe('validate', function() {
+  it('should download both source files and source maps', function(done) {
+    nock(host).get(path).reply(200, '//#sourceMappingURL=app.js.map');
 
-    nock(host)
-      .get('/static/app.js.map')
-      .reply(200, '{"version": 3}');
+    nock(host).get('/static/app.js.map').reply(200, '{"version": 3}');
 
-    validate(url, function (errors) {
+    validate(url, function(errors) {
       assert.equal(errors.length, 0);
       done();
     });
   });
 
-  it('should resolve absolute sourceMappiingURLs', function (done) {
+  it('should resolve absolute sourceMappingURLs', function(done) {
     nock(host)
       .get(path)
       .reply(200, '//#sourceMappingURL=https://127.0.0.1:8000/static/app.js.map');
 
-    nock('https://127.0.0.1:8000')
-      .get('/static/app.js.map')
-      .reply(200, '{"version": 3}');
+    nock('https://127.0.0.1:8000').get('/static/app.js.map').reply(200, '{"version": 3}');
 
-    validate(url, function (errors) {
+    validate(url, function(errors) {
       assert.equal(errors.length, 0);
       done();
     });
   });
 
-  it('should report a source file does not return 200', function (done) {
+  it('should resolve X-SourceMap headers', function(done) {
     nock(host)
       .get(path)
-      .reply(401, 'Not Authenticated');
+      .reply(200, 'function(){}();', {
+        'X-SourceMap': 'app.js.map'
+      });
 
-    validate(url, function (errors) {
+    nock(host).get('/static/app.js.map').reply(200, '{"version": 3}');
+
+    validate(url, function(errors) {
+      assert.equal(errors.length, 0);
+      done();
+    })
+  });
+
+  it('should resolve SourceMap headers', function(done) {
+    nock(host)
+      .get(path)
+      .reply(200, 'function(){}();', {
+        'SourceMap': 'app.js.map'
+      });
+
+    nock(host).get('/static/app.js.map').reply(200, '{"version": 3}');
+
+    validate(url, function(errors) {
+      assert.equal(errors.length, 0);
+      done();
+    })
+  });
+
+  it('should report a source file does not return 200', function(done) {
+    nock(host).get(path).reply(401, 'Not Authenticated');
+
+    validate(url, function(errors) {
       assert.equal(errors.length, 1);
       assert.equal(errors[0].constructor, UnableToFetchMinifiedError);
       done();
     });
   });
 
-  it('should report missing sourceMappingURL', function (done) {
-    nock(host)
-      .get(path)
-      .reply(200, 'function(){}();');
+  it('should report missing sourceMappingURL', function(done) {
+    nock(host).get(path).reply(200, 'function(){}();');
 
-    validate(url, function (errors) {
+    validate(url, function(errors) {
       assert.equal(errors.length, 1);
       assert.equal(errors[0].constructor, SourceMapNotFoundError);
       done();
     });
   });
 
-  it('should report a source map file does not return 200', function (done) {
-    nock(host)
-      .get(path)
-      .reply(200, '//#sourceMappingURL=app.js.map');
+  it('should report a source map file does not return 200', function(done) {
+    nock(host).get(path).reply(200, '//#sourceMappingURL=app.js.map');
 
-    nock(host)
-      .get('/static/app.js.map')
-      .reply(401, 'Not Authenticated');
+    nock(host).get('/static/app.js.map').reply(401, 'Not Authenticated');
 
-    validate(url, function (errors) {
+    validate(url, function(errors) {
       assert.equal(errors.length, 1);
       assert.equal(errors[0].constructor, UnableToFetchSourceMapError);
       done();
     });
   });
 
-  it('should report a source map file that does not parse', function (done) {
-      nock(host)
-      .get(path)
-      .reply(200, '//#sourceMappingURL=app.js.map');
+  it('should report a source map file that does not parse', function(done) {
+    nock(host).get(path).reply(200, '//#sourceMappingURL=app.js.map');
 
-    nock(host)
-      .get('/static/app.js.map')
-      .reply(200, '!@#(!*@#(*&@');
+    nock(host).get('/static/app.js.map').reply(200, '!@#(!*@#(*&@');
 
-    validate(url, function (errors) {
+    validate(url, function(errors) {
       assert.equal(errors.length, 1);
       assert.equal(errors[0].constructor, InvalidSourceMapFormatError);
       done();
@@ -102,18 +114,15 @@ describe('validate', function () {
   });
 });
 
-describe('errors', function () {
-  it('should stringify nicely', function () {
-    assert.deepEqual(
-      JSON.parse(JSON.stringify(new SourceMapNotFoundError(url))),
-      {
-        'name': 'SourceMapNotFoundError',
-        'message': 'Unable to locate a SourceMap in https://example.org/static/app.js',
-        'resolutions': [
-          'Add a <code>//# sourceMappingURL=</code> declaration',
-          'Add a SourceMap HTTP response header'
-        ]
-      }
-    );
+describe('errors', function() {
+  it('should stringify nicely', function() {
+    assert.deepEqual(JSON.parse(JSON.stringify(new SourceMapNotFoundError(url))), {
+      name: 'SourceMapNotFoundError',
+      message: 'Unable to locate a SourceMap in https://example.org/static/app.js',
+      resolutions: [
+        'Add a <code>//# sourceMappingURL=</code> declaration',
+        'Add a SourceMap HTTP response header'
+      ]
+    });
   });
 });
