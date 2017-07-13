220dcf6899fd8e71dc159b81dd846d7a92673b68
Ben Vinegar <ben@benv.ca>
2017-07-13 14:41:33 -0700

    Make examples work

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import Spinner from 'react-spinkit';
 
 import Loader from './Loader';
 
@@ -9,15 +8,39 @@ const VALIDATE_URL = process.env.REACT_APP_VALIDATE_URL;
 const TARGET_URL_PLACEHOLDER = 'http://code.jquery.com/jquery-1.9.1.min.js';
 
 function Example(props) {
-  let {children, url} = props;
+  let {name, url, version, onClick} = props;
   return (
     <li>
-      <a href={url}>
-        {children}
+      <a href={url} onClick={onClick}>
+        {name} ({version})
       </a>
     </li>
   );
 }
+
+const EXAMPLES = [
+  {
+    name: 'Underscore.js',
+    version: '1.8.3',
+    url: 'http://underscorejs.org/underscore-min.js'
+  },
+  {
+    name: 'Bootstrap.js',
+    version: '3.3.7',
+    url: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+  },
+  {
+    name: 'Raven.js',
+    version: '3.17.0',
+    url: 'https://cdn.ravenjs.com/3.17.0/raven.min.js'
+  },
+  {
+    name: 'AngularJS',
+    version: '1.5.6',
+    url: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js'
+  }
+];
+
 class Home extends Component {
   constructor() {
     super();
@@ -28,12 +51,13 @@ class Home extends Component {
   }
 
   handleSubmit(evt) {
+    evt.preventDefault();
+
     const url = this.state.targetUrl || TARGET_URL_PLACEHOLDER;
     const {history} = this.props;
 
     this.setState({loading: true});
 
-    evt.preventDefault();
     fetch(`${VALIDATE_URL}?url=${encodeURIComponent(url)}`, {
       method: 'POST'
     }).then(response => {
@@ -42,6 +66,15 @@ class Home extends Component {
       });
     });
   }
+
+  handleExampleClick(evt, url) {
+    evt.preventDefault();
+
+    this.setState({targetUrl: url}, () => {
+      this.handleSubmit(evt);
+    });
+  }
+
   render() {
     return this.state.loading
       ? <Loader />
@@ -65,24 +98,9 @@ class Home extends Component {
           </div>
           <h2>Examples</h2>
           <ul>
-            <Example url="/validate?url=http%3A%2F%2Fcode.jquery.com%2Fjquery-1.9.1.min.js">
-              jQuery
-            </Example>
-            <li>
-              <a href="/validate?url=http%3A%2F%2Fdocumentcloud.github.io%2Fbackbone%2Fbackbone-min.js">
-                Backbone.js
-              </a>
-            </li>
-            <li>
-              <a href="/validate?url=http%3A%2F%2Fcdn.ravenjs.com%2F1.1.0%2Fraven.min.js">
-                Raven.js
-              </a>
-            </li>
-            <li>
-              <a href="/validate?url=http%3A%2F%2Fgetsentry-cdn.s3.amazonaws.com%2Ftest%2Fraven.min.js">
-                Raven.js (with closure compiler)
-              </a>
-            </li>
+            {EXAMPLES.map(ex =>
+              <Example {...ex} onClick={(evt) => this.handleExampleClick(evt, ex.url)} />
+            )}
           </ul>
         </div>;
   }
