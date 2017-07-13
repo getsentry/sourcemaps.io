72aa4d86a5032c4ac06071c2283d5c3f502691d7
Ben Vinegar <ben@benv.ca>
2017-07-12 22:51:43 -0700

    Add react-router, split into home + report views ...
    ... and fetch + render report JSON.

---
+++ client/src/views/Report.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import queryString from 'query-string';
+
+function Entry(props) {
+  let {name, message, resolutions} = props;
+  return (
+    <li key={name}>
+      <h4>
+        {name}
+      </h4>
+      <p>
+        {message}
+      </p>
+      {resolutions &&
+        <div>
+          <h5>Resolutions</h5>
+          <ul>
+            {resolutions.map((res, i) =>
+              <li key={i}>
+                {res}
+              </li>
+            )}
+          </ul>
+        </div>}
+    </li>
+  );
+}
+class Report extends Component {
+  constructor(props) {
+    super(props);
+    const {reportUrl} = queryString.parse(props.location.search);
+    this.state = {reportUrl, report: null};
+  }
+
+  componentDidMount() {
+    fetch(this.state.reportUrl).then(response => {
+      response.json().then(report => {
+        this.setState({report});
+      });
+    });
+  }
+
+  render() {
+    const {report} = this.state;
+    return (
+      <div>
+        <h1>Report</h1>
+        {report &&
+          <div>
+            <h3>Sources</h3>
+            <ul>
+              {report.sources.map(src =>
+                <li key={src}>
+                  {src}
+                </li>
+              )}
+            </ul>
+            <h3>Errors</h3>
+            <ul>
+              {report.errors.map(Entry)}
+            </ul>
+            <h3>Warnings</h3>
+            {report.warnings.map(Entry)}
+            <ul />
+          </div>}
+      </div>
+    );
+  }
+}
+
+export default Report;
