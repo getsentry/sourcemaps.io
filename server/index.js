4b87692cd7ab929ef2599ba64d8be9a6fb975f82
Ben Vinegar <ben@benv.ca>
2017-07-12 17:03:57 -0700

    Return full public URL of report json file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const validate = require('./lib/validate');
-const Storage =  require('@google-cloud/storage');
+const Storage = require('@google-cloud/storage');
 
 let config = null;
 try {
@@ -21,32 +21,45 @@ const storage = Storage({
  * @param {object} event The Cloud Functions event.
  * @param {function} The callback function.
  */
-exports.validateSourceFile = function (req, res) {
-  res.set('Access-Control-Allow-Origin', '*')
-  res.set('Access-Control-Allow-Methods', 'POST')
+exports.validateSourceFile = function(req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'POST');
 
   const url = req.query.url;
   if (!url) {
     res.status(500).send('URL not specified');
   }
-  
-  validate(url, function (errors) {
+
+  validate(url, function(errors) {
     if (!errors || errors.length === 0) {
       res.status(200).end();
     }
 
     const bucket = storage.bucket(config.STORAGE_BUCKET);
 
+    // object names can't contain most symbols, so encode as a URI component
     const objectName = Date.now() + '_' + encodeURIComponent(url);
     const file = bucket.file(objectName);
 
     const stream = file.createWriteStream();
-    stream.on('error', (err) => {
+    stream.on('error', err => {
       res.status(500).send(err);
     });
     stream.on('finish', () => {
-      res.status(200).send(objectName);
+      res.status(200).send(
+        // need to encode a second time for inclusion as a URL
+        `https://storage.googleapis.com/${config.STORAGE_BUCKET}/${encodeURIComponent(
+          objectName
+        )}`
+      );
     });
-    stream.end(JSON.stringify(errors), 'utf8');
+
+    const report = {
+      url,
+      errors,
+      sources: [], // TODO
+      warnings: [] // TODO
+    };
+    stream.end(JSON.stringify(report), 'utf8');
   });
 };
