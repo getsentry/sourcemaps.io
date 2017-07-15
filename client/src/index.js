75e185ea4cfacb3a8f1519b5586e6e3311ef2cff
Ben Vinegar <ben@benv.ca>
2017-07-14 23:14:38 -0700

    Add Raven.js to client code / config via env var

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Raven.config(process.env.REACT_APP_SENTRY_DSN).install();
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
