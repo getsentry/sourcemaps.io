56c6f383af12e49e2bda835635c5e50ed3bfc6d1
Ben Vinegar <ben@benv.ca>
2017-08-05 17:10:52 -0700

    Express now serves local fixture data for testing

--- server/dev.js
+++ server/dev.js
@@ -2,6 +2,7 @@
  * Local test server for development
  */
 const express = require('express');
+const path = require('path');
 const {validateSourceFile} = require('.');
 
 const PORT = 3001;
@@ -9,6 +10,9 @@ const PORT = 3001;
 const app = express();
 
 app.post('/validateSourceFile', validateSourceFile);
+
+
+app.use('/fixtures', express.static(path.join('test', 'fixtures', 'build')));
 
 app.listen(PORT, () => {
   console.log(`sourcemaps.io local development server running on ${PORT}`);
