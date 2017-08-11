645a1de342231329ca79e349de2b9de894191faa
Ben Vinegar <ben@benv.ca>
2017-08-11 13:32:11 -0700

    Handle Uglify not mapping quoted strings correctly

--- server/lib/validate.js
+++ server/lib/validate.js
@@ -35,18 +35,31 @@ function validateMapping(mapping, sourceLines, generatedLines) {
     });
   }
 
-  const sourceToken = origLine.slice(
+  let sourceToken = origLine.slice(
     mapping.originalColumn,
     mapping.originalColumn + mapping.name.length
-  );
+  ).trim();
 
   // Token matches what we expect; everything looks good, bail out
-  if (sourceToken.trim() === mapping.name) {
+  if (sourceToken === mapping.name) {
+    return null;
+  }
+
+  // Start of token starts with a quote or apostrophe. This might be
+  // a bug in Uglify where it maps a token to the string of a token
+  // incorrectly - but it should still be fine for end users.
+  if (sourceToken.startsWith('\'') || sourceToken.startsWith('"')) {
+    sourceToken = origLine.slice(
+      mapping.originalColumn + 1,
+      mapping.originalColumn + mapping.name.length + 1
+    ).trim();
+  }
+
+  if (sourceToken === mapping.name) {
     return null;
   }
 
   const {generatedColumn} = mapping;
-
 
   let generatedLine;
   try {
--- server/test/fixtures/add.js
+++ server/test/fixtures/add.js
@@ -1,6 +1,11 @@
 // Input source file used to generate multiple different dist/map pairings.
 // See ../../webpack.config.js.
 
+var someMap = {
+  '1': 1,
+  '2': 2
+};
+
 /**
  * Usage:
  *   add(1, 2, 3) // returns 6
