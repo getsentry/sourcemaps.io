2abafc6d6dad1aa96bb9672b1738e6cdb6782870
Ben Vinegar <ben@benv.ca>
2017-08-13 19:13:21 -0700

    Don't attempt to test fixture files

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,6 @@
-require('./lib/__test__/fixtures/build-fixtures');
+require('./lib/__tests__/fixtures/build-fixtures');
 
 module.exports = {
+  testMatch: ['**/__tests__/**/*.spec.js?(x)'],
   rootDir: __dirname
 };
