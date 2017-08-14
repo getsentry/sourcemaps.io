b215daf6aa9f80ad25eb1757100bd4de6d732afb
Ben Vinegar <ben@benv.ca>
2017-08-13 19:11:29 -0700

    Move getSourceMapLocation to utils

--- server/lib/validateGeneratedFile.js
+++ server/lib/validateGeneratedFile.js
@@ -9,38 +9,8 @@ const {
   ResourceTimeoutError
 } = require('./errors');
 const {MAX_TIMEOUT} = require('./constants');
-const {resolveUrl} = require('./utils');
+const {resolveUrl, getSourceMapLocation} = require('./utils');
 
-
-function getSourceMapLocation(response, body) {
-  // First, look for Source Map HTTP headers
-  const sourceMapHeader =
-    response.headers['x-sourcemap'] || response.headers.sourcemap;
-
-  if (sourceMapHeader) return sourceMapHeader;
-
-  // If no headers, look for a sourceMappingURL directive on the last line
-  const lines = body.split(/\n/);
-  if (!lines.length > 0) {
-    return null;
-  }
-
-  // consider anything in last 5 lines; browsers and tools like sentry.io
-  // are similarly generous
-  const last = lines.slice(-5);
-  const DIRECTIVE_RE = /sourceMappingURL=(\S+)$/;
-
-  let line;
-  let match;
-
-  while (last.length) {
-    line = last.pop();
-    match = line.match(DIRECTIVE_RE);
-    if (match) return match[1];
-  }
-
-  return null;
-}
 
 /**
  * Validates a target transpiled/minified file located at a given url
