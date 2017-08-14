89a5587c400e85d6772e9a1deaa77c07bebb3edc
Ben Vinegar <ben@benv.ca>
2017-08-13 18:53:52 -0700

    Seperate into matching test files

---
+++ server/lib/__test__/validateMappings.spec.js
@@ -0,0 +1,26 @@
+const validateMappings = require('../validateMappings');
+
+describe('validateMappings', () => {
+  it('should stop at 100 errors', () => {
+    const sourceMapConsumer = {
+      eachMapping(callback) {
+        // mock source map consumer with 200 entries;
+        // each one should fail
+        for (let i = 0; i < 200; i++) {
+          callback({
+            source: 'app.js',
+            name: 'foo',
+            originalLine: 10,
+            originalColumn: 10
+          });
+        }
+      },
+      sourceContentFor() {
+        return 'lol();';
+      }
+    };
+    // assert `validateMappings` stopped at 100 entries
+    const report = validateMappings(sourceMapConsumer);
+    expect(report.errors).toHaveLength(100);
+  });
+});
