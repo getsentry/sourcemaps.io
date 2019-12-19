3a297460bcb46f56201567365338bf5add7fec6c
Ben Vinegar <ben@sentry.io>
2019-12-18 16:33:32 -0800

    First pass at types for errors.ts

--- server/src/lib/errors.ts
+++ server/src/lib/errors.ts
@@ -1,103 +1,134 @@
-
-function SourceMapNotFoundError(url) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = `Unable to locate a source map in ${url}`;
-  this.resolutions = [
-    'Add a <code>//# sourceMappingURL=</code> declaration',
-    'Add a SourceMap HTTP response header'
-  ];
-}
-
-function UnableToFetchError(url) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = `Unable to fetch ${url}`;
-  this.resolutions = ['Is your URL correct?'];
-}
-
-function UnableToFetchSourceError(url) {
-  UnableToFetchError.call(this, url);
-}
-
-function UnableToFetchMinifiedError(url) {
-  UnableToFetchError.call(this, url);
-}
-
-function UnableToFetchSourceMapError(url) {
-  UnableToFetchError.call(this, url);
-  this.resolutions = ['SourceMap declaration found, but could not load the file.'];
-}
-
-function InvalidJSONError(url, error) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  let message = 'Does not parse as JSON';
-  if (error && error.message) {
-    message += `: ${error.message}`;
+class SourceMapNotFoundError extends Error {
+  resolutions: Array<string>;
+  constructor(url: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = `Unable to locate a source map in ${url}`;
+    this.resolutions = [
+      'Add a <code>//# sourceMappingURL=</code> declaration',
+      'Add a SourceMap HTTP response header'
+    ];
   }
-  this.message = message;
-  this.resolutions = ['Everything is broken. Is this really a Source Map?'];
 }
 
-function InvalidSourceMapFormatError(url, error) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  let message = 'Invalid SourceMap format';
-  if (error && error.message) {
-    message += `: ${error.message}`;
+class UnableToFetchError extends Error {
+  resolutions: Array<string>;
+  constructor(url: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = `Unable to fetch ${url}`;
+    this.resolutions = ['Is your URL correct?'];
   }
-  this.message = message;
-
-  this.resolutions = ['Everything is broken. Is this really a Source Map?'];
 }
 
-function ResourceTimeoutError(url, duration) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = `Resource timed out (exceeded ${duration}ms): ${url}`;
-  this.resolutions = ['Is your URL correct?'];
+class UnableToFetchSourceError extends UnableToFetchError {};
+class UnableToFetchMinifiedError extends UnableToFetchError {};
+class UnableToFetchSourceMapError extends UnableToFetchError {
+  constructor(url: string) {
+    super(url);
+    this.resolutions = ['SourceMap declaration found, but could not load the file.'];
+  }
+};
+
+class InvalidJSONError extends Error {
+  resolutions: Array<string>;
+
+  constructor(url: string, error: Error) {
+    super();
+
+    this.name = this.constructor.name;
+    let message: string = 'Does not parse as JSON';
+    if (error && error.message) {
+      message += `: ${error.message}`;
+    }
+    this.message = message;
+    this.resolutions = ['Everything is broken. Is this really a Source Map?'];
+  }
 }
 
-function BadContentError(url) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = `File is not JavaScript: ${url}`;
-  this.resolutions = [
-    'Is this URL accessible externally?',
-    'Is this URL accessible without a user session?'
-  ];
+class InvalidSourceMapFormatError extends Error {
+  resolutions: Array<string>;
+  constructor(url: string, error: Error) {
+    super();
+
+    this.name = this.constructor.name;
+    let message = 'Invalid SourceMap format';
+    if (error && error.message) {
+      message += `: ${error.message}`;
+    }
+    this.message = message;
+  
+    this.resolutions = ['Everything is broken. Is this really a Source Map?'];
+  }
 }
 
-function LineNotFoundError(source, options) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.source = source;
+class ResourceTimeoutError extends Error {
+  resolutions: Array<string>;
 
-  const {line, column} = options;
-  this.line = line;
-  this.column = column;
-  this.message = 'Line not found in source file';
-  this.resolutions = [];
+  constructor(url: string, duration: number) {
+    super();
+    this.name = this.constructor.name;
+    this.message = `Resource timed out (exceeded ${duration}ms): ${url}`;
+    this.resolutions = ['Is your URL correct?'];    
+  }
 }
 
-function BadTokenError(source, options) {
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.source = source;
+class BadContentError extends Error {
+  resolutions: Array<string>;
 
-  const {token, expected, mapping} = options;
-  this.token = token;
-  this.expected = expected;
-  this.mapping = mapping;
-
-  this.message = 'Expected token not in correct location';
-  this.resolutions = [];
+  constructor(url: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = `File is not JavaScript: ${url}`;
+    this.resolutions = [
+      'Is this URL accessible externally?',
+      'Is this URL accessible without a user session?'
+    ];
+  }
 }
 
-function BadColumnError(source, options) {
-  BadTokenError.call(this, source, options);
+class LineNotFoundError extends Error {
+  source: string;
+  column: number;
+  line: number;
+  resolutions: Array<string>;
+
+  constructor(source: string, options: {line: number, column: number}) {
+    super();
+    this.name = this.constructor.name;
+    this.source = source;
+  
+    const {line, column} = options;
+    this.line = line;
+    this.column = column;
+    this.message = 'Line not found in source file';
+    this.resolutions = [];
+  }
 }
+
+class BadTokenError extends Error {
+  source: string;
+  token: string;
+  expected: string;
+  mapping: string
+  resolutions: Array<string>;
+
+  constructor(source: string, options: {token: string, expected: string, mapping: string}) {
+    super();
+    this.name = this.constructor.name;
+    this.source = source;
+  
+    const {token, expected, mapping} = options;
+    this.token = token;
+    this.expected = expected;
+    this.mapping = mapping;
+  
+    this.message = 'Expected token not in correct location';
+    this.resolutions = [];
+  }
+}
+
+class BadColumnError extends Error {};
 
 module.exports = {
   SourceMapNotFoundError,
