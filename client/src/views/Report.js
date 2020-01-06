5eaf03ed3f58f7a77ec4fc292b2489aa20f26c5f
Ben Vinegar <ben@sentry.io>
2020-01-06 13:04:13 -0800

    fix(client/build): Upgrade eslint to 6.x, fix lint errors

--- client/src/views/Report.js
+++ client/src/views/Report.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactTooltip from 'react-tooltip';
 import PropTypes from 'prop-types';
 
@@ -10,7 +10,7 @@ const STORAGE_URL = process.env.REACT_APP_STORAGE_URL;
  * Error entry for token/mapping errors (includes source code context)
  */
 function BadTokenEntry(props, index) {
-  const {source, expected, token, mapping} = props;
+  const { source, expected, token, mapping } = props;
   const {
     originalContext,
     originalLine,
@@ -35,15 +35,14 @@ function BadTokenEntry(props, index) {
       <div>
         <pre className="code">
           <ol start={originalContext[0][0]}>
-            {originalContext.map(([line, ctx]) =>
+            {originalContext.map(([line, ctx]) => (
               <li
                 className={line === originalLine ? 'line line-active' : 'line'}
-                key={line}>
-                <span>
-                  {ctx}
-                </span>
+                key={line}
+              >
+                <span>{ctx}</span>
               </li>
-            )}
+            ))}
           </ol>
         </pre>
       </div>
@@ -62,25 +61,26 @@ BadTokenEntry.propTypes = {
  * Generic error entry
  */
 function Entry(props, index) {
-  const {name, message, resolutions} = props;
+  const { name, message, resolutions } = props;
 
-  const htmlMessage = (message || '').replace(/(https?\S+)/g, '<a href="$1">$1</a>');
+  const htmlMessage = (message || '').replace(
+    /(https?\S+)/g,
+    '<a href="$1">$1</a>'
+  );
   return (
     <li className="entry" key={index}>
-      <h4>
-        {name}
-      </h4>
-      {message && <p dangerouslySetInnerHTML={{__html: htmlMessage}} />}
-      {resolutions &&
-        resolutions.length > 0 &&
+      <h4>{name}</h4>
+      {message && <p dangerouslySetInnerHTML={{ __html: htmlMessage }} />}
+      {resolutions && resolutions.length > 0 && (
         <div>
           <h5>Resolutions</h5>
           <ul>
-            {resolutions.map((res, i) =>
-              <li key={i} dangerouslySetInnerHTML={{__html: res}} />
-            )}
+            {resolutions.map((res, i) => (
+              <li key={i} dangerouslySetInnerHTML={{ __html: res }} />
+            ))}
           </ul>
-        </div>}
+        </div>
+      )}
     </li>
   );
 }
@@ -91,7 +91,7 @@ Entry.propTypes = {
   resolutions: PropTypes.array,
   source: PropTypes.string,
   column: PropTypes.number,
-  line: PropTypes.line,
+  line: PropTypes.number,
   expected: PropTypes.string,
   token: PropTypes.string
 };
@@ -100,22 +100,22 @@ class Report extends Component {
   constructor(props) {
     super(props);
     const reportName = props.match.params.report;
-    this.state = {reportName, report: null};
+    this.state = { reportName, report: null };
   }
 
   componentDidMount() {
-    const {reportName} = this.state;
+    const { reportName } = this.state;
     const reportUrl = `${STORAGE_URL}/${encodeURIComponent(reportName)}`;
 
     fetch(reportUrl).then((response) => {
       response.json().then((report) => {
-        this.setState({report});
+        this.setState({ report });
       });
     });
   }
 
   renderAlert() {
-    const {report} = this.state;
+    const { report } = this.state;
     if (!report) return null;
 
     if (report.errors.length === 0 && report.warnings.length === 0) {
@@ -139,77 +139,88 @@ class Report extends Component {
   }
 
   render() {
-    const {report} = this.state;
-    return !report
-      ? <Loader />
-      : <div>
+    const { report } = this.state;
+    return !report ? (
+      <Loader />
+    ) : (
+      <div>
         {this.renderAlert()}
         <h2>
-            Report for <a href={report.url}>{report.url.split('/').pop()}</a>
+          Report for <a href={report.url}>{report.url.split('/').pop()}</a>
         </h2>
-        {report &&
-            <div>
-              <h3>URL</h3>
-              <ul><li><a href={report.url}>{report.url}</a></li></ul>
-              {report.sourceMap &&
-                <div>
-                  <h3>Source Map</h3>
-                  <ul>
-                    <li>
-                      <a href={report.sourceMap}>
-                        {report.sourceMap}
-                      </a>
+        {report && (
+          <div>
+            <h3>URL</h3>
+            <ul>
+              <li>
+                <a href={report.url}>{report.url}</a>
+              </li>
+            </ul>
+            {report.sourceMap && (
+              <div>
+                <h3>Source Map</h3>
+                <ul>
+                  <li>
+                    <a href={report.sourceMap}>{report.sourceMap}</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+            {report.sources.length > 0 && (
+              <div>
+                <h3>
+                  Sources{' '}
+                  <span className="badge badge-success">
+                    {report.sources.length}
+                  </span>
+                </h3>
+                <ul>
+                  {report.sources.map(src => (
+                    <li key={src}>
+                      <a href={src}>{src}</a>
                     </li>
-                  </ul>
-                </div>}
-              {report.sources.length > 0 &&
-                <div>
-                  <h3>
-                    Sources{' '}
-                    <span className="badge badge-success">{report.sources.length}</span>
-                  </h3>
-                  <ul>
-                    {report.sources.map(src =>
-                      <li key={src}>
-                        <a href={src}>
-                          {src}
-                        </a>
-                      </li>
-                    )}
-                  </ul>
-                </div>}
-              <h3>
-                Errors{' '}
-                {report.errors.length > 0
-                  ? <span className="badge" style={{background: '#b94a48'}}>
-                    {report.errors.length}
-                  </span>
-                  : <span className="badge">0</span>}
-              </h3>
-              <ul>
-                {report.errors.map((err, index) => {
-                  return ['BadTokenError', 'BadColumnError'].includes(err.name)
-                    ? BadTokenEntry(err, index)
-                    : Entry(err, index);
-                })}
-              </ul>
-              <h3>
-                Warnings{' '}
-                {report.warnings.length > 0
-                  ? <span className="badge" style={{background: '#f89406'}}>
-                    {report.warnings.length}
-                  </span>
-                  : <span className="badge">0</span>}
-              </h3>
-              <ul>
-                {report.warnings.map((err, index) => {
-                  return ['BadTokenError', 'BadColumnError'].includes(err.name)
-                    ? BadTokenEntry(err, index)
-                    : Entry(err, index);
-                })}
-              </ul>
-            </div>}
-      </div>;
+                  ))}
+                </ul>
+              </div>
+            )}
+            <h3>
+              Errors{' '}
+              {report.errors.length > 0 ? (
+                <span className="badge" style={{ background: '#b94a48' }}>
+                  {report.errors.length}
+                </span>
+              ) : (
+                <span className="badge">0</span>
+              )}
+            </h3>
+            <ul>
+              {report.errors.map((err, index) => {
+                return ['BadTokenError', 'BadColumnError'].includes(err.name)
+                  ? BadTokenEntry(err, index)
+                  : Entry(err, index);
+              })}
+            </ul>
+            <h3>
+              Warnings{' '}
+              {report.warnings.length > 0 ? (
+                <span className="badge" style={{ background: '#f89406' }}>
+                  {report.warnings.length}
+                </span>
+              ) : (
+                <span className="badge">0</span>
+              )}
+            </h3>
+            <ul>
+              {report.warnings.map((err, index) => {
+                return ['BadTokenError', 'BadColumnError'].includes(err.name)
+                  ? BadTokenEntry(err, index)
+                  : Entry(err, index);
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
