2b0d882985f3a4f31d1f84e8cdb8cff88baf6d3c
Ben Vinegar <ben@sentry.io>
2020-01-07 12:56:51 -0800

    feat: Use ts-jest preprocessor (and drop janky rsync build)

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  testMatch: [path.join(__dirname, '/dist/**/__tests__/**/*.spec.js?(x)')],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [path.join(__dirname, '/src/**/__tests__/**/*.spec.{js,ts}')],
   setupFiles: [
-    path.join(__dirname, '/dist/lib/__tests__/fixtures/build-fixtures')
+    path.join(__dirname, '/src/lib/__tests__/fixtures/build-fixtures')
   ],
   rootDir: __dirname
 };
