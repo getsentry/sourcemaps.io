eac90c959e6a5a12c892369b8e523474dbf47df9
Ben Vinegar <ben@sentry.io>
2020-05-16 16:56:40 -0700

    Upgrade to @sentry/browser 5.x

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Raven from 'raven-js';
+import * as Sentry from '@sentry/browser';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 if (process.env.REACT_APP_SENTRY_DSN) {
-  Raven.config(process.env.REACT_APP_SENTRY_DSN, {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_GIT_SHA
-  }).install();
+  });
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
