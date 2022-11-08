663dbfb5a1134c619b82b90697cd26c3af5b8f8c
Billy Vong <billyvg@users.noreply.github.com>
2022-11-08 18:46:35 -0500

    feat: Add `tracingOrigins`

--- client/src/index.js
+++ client/src/index.js
@@ -12,9 +12,14 @@ if (process.env.REACT_APP_SENTRY_DSN) {
     debug: true,
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_GIT_SHA,
-    integrations: [new BrowserTracing(), new Replay({
-      sessionSampleRate: 1.0
-    })],
+    integrations: [
+      new BrowserTracing({
+        tracingOrigins: ['sourcemaps.io']
+      }),
+      new Replay({
+        sessionSampleRate: 1.0
+      })
+    ],
     tracesSampleRate: 1.0
   });
 }
