3d225d0def448c7c94d652e1484425048e3261b2
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-03-14 11:18:36 -0700

    wip: Deploy with GH Actions

--- .github/sentry-release.sh
+++ .github/sentry-release.sh
@@ -6,8 +6,8 @@ if [ "${SENTRY_AUTH_TOKEN}" != "" ]; then
     SENTRY_AUTH_TOKEN_PUBLIC="<hidden>"
 fi
 
-if [ -z "${TRAVIS_COMMIT}" ]; then
-    TRAVIS_COMMIT=`git rev-parse HEAD`
+if [ -z "${GITHUB_SHA}" ]; then
+    GITHUB_SHA=$(git rev-parse HEAD)
 fi
 
 echo "SENTRY_ORG: ${SENTRY_ORG}"
@@ -15,7 +15,7 @@ echo "SENTRY_AUTH_TOKEN: ${SENTRY_AUTH_TOKEN_PUBLIC}"
 echo "SENTRY_SERVER_PROJECT: ${SENTRY_SERVER_PROJECT}"
 echo "SENTRY_CLIENT_PROJECT: ${SENTRY_CLIENT_PROJECT}"
 echo "SOURCEMAPSIO_REPO: ${SOURCEMAPSIO_REPO}"
-echo "TRAVIS_COMMIT: ${TRAVIS_COMMIT}"
+echo "GITHUB_SHA: ${GITHUB_SHA}"
 
 curl https://sentry.io/api/0/organizations/${SENTRY_ORG}/releases/ \
   -X POST \
@@ -23,10 +23,10 @@ curl https://sentry.io/api/0/organizations/${SENTRY_ORG}/releases/ \
   -H 'Content-Type: application/json' \
   -d "
 {
-    \"version\": \"${TRAVIS_COMMIT}\",
+    \"version\": \"${GITHUB_SHA}\",
     \"refs\": [{
         \"repository\":\"${SOURCEMAPSIO_REPO}\",
-        \"commit\":\"${TRAVIS_COMMIT}\"
+        \"commit\":\"${GITHUB_SHA}\"
     }],
     \"projects\":[\"${SENTRY_SERVER_PROJECT}\", \"${SENTRY_CLIENT_PROJECT}\"]
 }"
